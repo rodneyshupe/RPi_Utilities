728661b79e2654a600cb925abbd0da9fa44fdc70
Rodney Shupe <rodney@shupe.ca>
2020-01-24 18:00:46 -0800

    fixed conditional logic

--- files/bashrc_insert_prompt.sh
+++ files/bashrc_insert_prompt.sh
@@ -1,4 +1,6 @@
 
-if [ "$color_prompt" = yes ]; then
-    PS1='$(if [[ $? == 0 ]]; then  echo "\[\e[30;46m\] \@ \[\e[m\] \[\e[32m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\[\e[0;33m\][\w]\[\e[m\]\$ "; else echo "\[\e[30;46m\] \@ \[\e[m\] \[\e[32m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\[\e[0;33m\][\w]\[\e[m\]\[\e[41m\]($?)\[\e[m\]$ "; fi)'
-fi
+case "$TERM" in
+    xterm-color|*-256color)
+      PS1='$(if [[ $? == 0 ]]; then  echo "\[\e[30;46m\] \@ \[\e[m\] \[\e[32m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\[\e[0;33m\][\w]\[\e[m\]\$ "; else echo "\[\e[30;46m\] \@ \[\e[m\] \[\e[32m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:\[\e[0;33m\][\w]\[\e[m\]\[\e[41m\]($?)\[\e[m\]$ "; fi)'
+      ;;
+esac
